354daec52fa9e336a54486484012f04be20c1a0d
Arpan Raval <arpanraval1112@gmail.com>
2024-07-30 23:43:48 +0530

    Update iti.js

--- iti.js
+++ iti.js
@@ -104,3 +104,22 @@ document.addEventListener("contextmenu",function(event){
     alert("Right-clicking has been disabled by Mr. Arpan Raval, the developer.");
     event.preventDefault();
 });
+
+document.addEventListener('keydown', function(event) {
+    // Disable F12
+    if (event.keyCode == 123) {
+        event.preventDefault();
+    }
+    // Disable Ctrl+Shift+I
+    else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
+       event.preventDefault();
+    }
+    // Disable Ctrl+Shift+J
+    else if (event.ctrlKey && event.shiftKey && event.keyCode == 74) {
+        event.preventDefault();
+    }
+    // Disable Ctrl+U
+    else if (event.ctrlKey && event.keyCode == 85) {
+       event.preventDefault();
+    }
+});
